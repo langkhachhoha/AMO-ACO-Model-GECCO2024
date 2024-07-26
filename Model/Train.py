cb668db9119ebaa5dc657b406fb63cb24ded156d
Hieu <haminhhieu1005v@gmail.com>
2024-07-26 11:50:15 +0700

    abc

--- Model/Train.py
+++ Model/Train.py
@@ -6,7 +6,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="AMO-ACO")
     parser.add_argument("--graph_size", type=int, default=100, help="Number of nodes in the graph")
     parser.add_argument("--data_path", type=str, default="Data/", help="the root folder of the data")
-    parser.add_argument("--epochs", default=500, type=int, help="Total number of epochs")
+    parser.add_argument("--epochs", default=200, type=int, help="Total number of epochs")
     parser.add_argument("--lr", default=0.001, type=float, help="initial learning rate")
     parser.add_argument("--n_ants", default=20, type=int, help="number of ants")
     parser.add_argument("--steps_per_epoch", default=100, type=int, help="number of steps per epoch")
